bb0d325e87b492da305831ab7ec8bcadd24a9108
pyramixofficial <balamatiuc2@gmail.com>
2023-09-10 00:22:37 +0300

    Adapted the function to provide only sets of links and created the links differently

--- tools.py
+++ tools.py
@@ -1,5 +1,24 @@
 import time
 
+def process_llm_output(
+    the_answer: dict,
+):
+    """
+    This function will process the output dictionary output of the model to combine the text and the 
+    links in an appropriate way to be displayed to the user.
+
+    Args:
+        the_answer (dict): The answer from the model
+
+    Returns:
+        main_text (str): The main text of the answer
+        links (list): The unique links of the answer 
+        
+    """
+    main_text = the_answer["message"]
+    links = the_answer["links"]
+    return main_text, list(set(links))
+
 
 def generate_response(
     prompt_input: str,
@@ -17,30 +36,8 @@ def generate_response(
     # some dark magic should happen here
     # and a function will do something about the prompt aferwards generating the response
     # in an answer variable that will be returned
-    delay = 10
-    time.sleep(delay)
     the_answer = {
-        "message": f"This is the output the message that will contain the main idea of the project",
-        "links": ["https://www.google.com", "https://www.google.com", "https://www.google.com"]
+        "message": f"{prompt_input} - This is the output the message that will contain the main idea of the project{prompt_input}",
+        "links": ["https://www.google.com", "https://doogle.com", "https://www.googcom"]
     }
-    return the_answer
-
-
-def process_llm_output(
-    the_answer: dict,
-):
-    """
-    This function will process the output dictionary output of the model to combine the text and the 
-    links in an appropriate way to be displayed to the user.
-
-    Args:
-        the_answer (dict): The answer from the model
-
-    Returns:
-        main_text (str): The main text of the answer
-        links (list): The links of the answer
-        
-    """
-    main_text = the_answer["message"]
-    links = the_answer["links"]
-    return main_text, links
+    return process_llm_output(the_answer)
